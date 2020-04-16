69bfb10a1416ff8d1c6e350f452fa60835e6b519
axs817 <63751292+axs817@users.noreply.github.com>
2020-04-16 16:10:05 -0400

    Adding CreatingFrame.m
    This function creates an image frame with the dye molecules based on the provided parameters .

---
+++ CreatingFrame.m
@@ -0,0 +1,14 @@
+function [frame]=CreatingFrame(A,r,px,sigmax,m,n)
+
+for k = 1:r
+    for i = 1:px
+        for j=1:px
+        frame0(i,j)=A*exp(-((i-m(k))^2/(2*sigmax^2)+(j-n(k))^2/(2*sigmax^2)))*randi([9000 11000])/10000;
+            if k==1
+        frame(i,j)=frame0(i,j);
+    else
+        frame(i,j)=frame0(i,j)+frame(i,j);
+    end
+        end
+    end
+end
